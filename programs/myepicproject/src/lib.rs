e0602951ba9c089b536869c8f62f465a188763c5
afzaanhakim <afzaanhakim19@gmail.com>
2021-11-22 12:42:58 -0500

    code in rust to check gifs and updating main function in js to use the correct programs from anchor, set up base account and logic to get total gifs

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -8,9 +8,28 @@ pub mod myepicproject { //pub mod tells basically  this is a rust module
   use super::*;
   //start_stuff_off is like a function declaration which uses context as param
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+       // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account; //&mut is a mutable reference to base account
+    // Initialize total_gifs.
+    base_account.total_gifs = 0;
     Ok(())
   }
 }
 
+
+//attaching variables to the startstuffoff context
 #[derive(Accounts)] //another macro
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+#[account(init, payer = user, space = 9000)] // telling solana how we want to initialize base account, init: tells solana to create a new account, payer : who is paying for the account to be created, basically the user calling this function, space: 9000bytes being allocated to the pgram
+pub base_account: Account<'info, BaseAccount>,
+#[account(mut)] //mut means mutable reference
+pub user: Signer<'info>,
+pub system_program: Program<'info, System>,//reference to the program that is running solana 
+
+}
+
+// Tell Solana what we want to store on this account. basically base account holds integers namely count of total_gifs
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,29 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3; //system program
 
 const main = async () => {
   console.log("INITIALIZING TEST");
-
-  anchor.setProvider(anchor.Provider.env()); //setting provider for anchor
+const provider = anchor.Provider.env(); //creating and setting a provider before we update it
+  anchor.setProvider(provider); //setting provider for anchor
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
 
-  console.log("transaction signatoooor", tx);
+//declare a baseaccount and store the newly created keypair for the program to use --- anchor.web3.Keypair.generate() Creates the credentials for the baseaccount
+  const baseAccount = anchor.web3.Keypair.generate();
+  const tx = await program.rpc.startStuffOff({
+accounts : {
+  baseAccount: baseAccount.publicKey,
+  user:provider.wallet.publicKey,
+  systemProgram: SystemProgram.programId
+},
+signers:[baseAccount],
+
+
+  });
+
+  console.log(" ✍️ ✍️ ✍️ transaction signatoooor ✍️ ✍️ ✍️✍️ ✍️ ✍️", tx);
+
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 THIS IS THE GIF COUNT', account.totalGifs.toString())
 };
 
 const runMain = async () => {
