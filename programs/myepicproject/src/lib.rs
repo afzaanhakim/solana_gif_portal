b2cb10fccf6c73c22343061720ab14ca78a8e5ef
afzaanhakim <afzaanhakim19@gmail.com>
2021-11-22 13:07:25 -0500

    declaring type of links in structs as String adding new item to the list of gifs and posting that data + getting the new list with url of gif and signer  address

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -14,10 +14,19 @@ pub mod myepicproject { //pub mod tells basically  this is a rust module
     base_account.total_gifs = 0;
     Ok(())
   }
-
-  pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+//making function a gif link param from user
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {
 
     let base_account = &mut ctx.accounts.base_account;
+    let user = &mut ctx.accounts.user;
+
+    //struct
+    let item = ItemStruct {
+        gif_link:gif_link.to_string(),
+        user_address: *user.to_account_info().key
+    };
+
+    base_account.gif_list.push(item);
     base_account.total_gifs += 1;
     Ok(())
   }
@@ -37,15 +46,27 @@ pub system_program: Program<'info, System>,//reference to the program that is ru
 
 
 //creating a struct to specifiy data i want in addgif that has a mutable reference to base_account
-
+//also added the signer who calls the addGif method
 #[derive(Accounts)]
 pub struct AddGif<'info>{
     #[account(mut)]
     pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
 }
+
+// Create a custom struct to work with. - holds the giflink as a string and the users_address
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)] //basically how anchor can serilize or deserialize the struct
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
 
 // Tell Solana what we want to store on this account. basically base account holds integers namely count of total_gifs
 #[account]
 pub struct BaseAccount {
     pub total_gifs: u64,
+    // a Vector (basically an array) of type ItemStruct attached to the account.
+    pub gif_list: Vec<ItemStruct>
 }
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -3,40 +3,40 @@ const { SystemProgram } = anchor.web3; //system program
 
 const main = async () => {
   console.log("INITIALIZING TEST");
-const provider = anchor.Provider.env(); //creating and setting a provider before we update it
+  const provider = anchor.Provider.env(); //creating and setting a provider before we update it
   anchor.setProvider(provider); //setting provider for anchor
   const program = anchor.workspace.Myepicproject;
 
-//declare a baseaccount and store the newly created keypair for the program to use --- anchor.web3.Keypair.generate() Creates the credentials for the baseaccount
+  //declare a baseaccount and store the newly created keypair for the program to use --- anchor.web3.Keypair.generate() Creates the credentials for the baseaccount
   const baseAccount = anchor.web3.Keypair.generate();
   const tx = await program.rpc.startStuffOff({
-accounts : {
-  baseAccount: baseAccount.publicKey,
-  user:provider.wallet.publicKey,
-  systemProgram: SystemProgram.programId
-},
-signers:[baseAccount],
-
-
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
   });
 
   console.log(" ✍️ ✍️ ✍️ transaction signatoooor ✍️ ✍️ ✍️✍️ ✍️ ✍️", tx);
 
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 THIS IS THE GIF COUNT', account.totalGifs.toString());
-
+  console.log("👀 THIS IS THE GIF COUNT", account.totalGifs.toString());
 
   //calling-addgif
 
-  await program.rpc.addGif({
+  await program.rpc.addGif("https://media.giphy.com/media/GRSnxyhJnPsaQy9YLn/giphy.gif",{
     accounts: {
       baseAccount: baseAccount.publicKey,
-    }
-  })
+      user: provider.wallet.publicKey,
+    },
+  });
 
-//calling account again to get newGif count after using addGif
+  //calling account again to get newGif count after using addGif
   account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 THIS IS THE new GIF COUNT', account.totalGifs.toString());
+  console.log("👀 THIS IS THE new GIF COUNT", account.totalGifs.toString());
+
+  console.log("👀👀👀THIS IS THE GIF LIST👀👀👀", account.gifList)
 };
 
 const runMain = async () => {
